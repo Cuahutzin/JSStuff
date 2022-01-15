e9029ff5708b5f86928e81ac09659124c96bbccc
Eduardo Chavez <eduardoc.chavezh@gmail.com>
2022-01-15 14:38:14 -0600

    includes and double asterisc es7

---
+++ ecmascript/src/es7/index.js
@@ -0,0 +1,15 @@
+console.log("asd");
+
+let numbers = [1,2,3,7,8];
+//Before we use index of
+if(numbers.includes(7)){
+    console.log('Si se encuentra el valor 7');
+}else{
+    console.log('No se encuentra');
+}
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
