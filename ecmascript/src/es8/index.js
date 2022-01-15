f88d215e12ed1248bb9397c0668ca5e76bd08272
Eduardo Chavez <eduardoc.chavezh@gmail.com>
2022-01-15 15:39:21 -0600

    Async await

--- ecmascript/src/es8/index.js
+++ ecmascript/src/es8/index.js
@@ -36,3 +36,29 @@ const data3= {
     front:'Alej', 
     back: 'Rel',//could  exist another element, or not
 }
+
+//Async and await
+const helloWorld = () =>{
+    return new Promise ((resolve, reject) => {
+        (false)
+        ? setTimeout(() => resolve("Hello World"), 3000)
+        :reject(new Error('Test Error'))
+    })
+};
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+helloAsync();
+
+const anotherFunction = async() => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+anotherFunction();
