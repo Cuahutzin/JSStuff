b58c59e604564bb52c1325fd86149b976a43ebc4
Eduardo Chavez <eduardoc.chavezh@gmail.com>
2022-01-15 18:32:38 -0600

    fun tion scope

---
+++ src/scope-closure/scope/function.js
@@ -0,0 +1,14 @@
+const fruits = () => {
+    var fruit = 'apple';
+    console.log(fruit);
+};
+
+fruits();
+// console.log(fruit); // not defined
+
+const anotherFunction = () => {
+    var x = 1;
+    var x = 2;
+    let y = 1;
+    let y = 2;
+}
