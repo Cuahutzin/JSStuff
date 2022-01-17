9d88c16f250babd975c309a73cc8dfbb9ac901b4
Eduardo Chávez <che1ga@bosch.com>
2022-01-17 11:34:29 -0600

    Private variables with closures

---
+++ src/scope-closure/closure/private.js
@@ -0,0 +1,18 @@
+// private variables with closure
+
+const person = () => {
+    var saveName = "Name";
+    return {
+        getName: () => {
+            return saveName;
+        },
+        setName: (name) => {
+            saveName = name;
+        },
+    };
+};
+
+newPerson = person();
+console.log ( newPerson.getName());
+newPerson.setName('Oscar');
+console.log(newPerson.getName());
