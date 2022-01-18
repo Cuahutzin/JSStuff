a3e2ed2e068c920c8e9fa0b3efc62c0a3dbd3016
Eduardo Chávez <che1ga@bosch.com>
2022-01-18 12:45:19 -0600

    .

--- src/asynchronism/utils/fetchData.js
+++ src/asynchronism/utils/fetchData.js
@@ -6,7 +6,7 @@ const fetchData = (url_api) =>{
         const xhttp = new XMLHttpRequest();
         xhttp.open('GET',url_api, true);
         xhttp.onreadystatechange = (() =>{
-            if(xhttp.readyState === 4){//Is it completes?
+            if(xhttp.readyState === 4){//Is it completed?
                 (xhttp.status === 200)
                     ? resolve(JSON.parse(xhttp.responseText))
                     : reject(new Error('Error ' + url_api))
